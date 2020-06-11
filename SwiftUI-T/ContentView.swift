6bbbfe8b89f757e6079fa5b949bc1d0e7a22c19a
fengjutian <2640111081@qq.com>
2020-06-11 19:21:47 +0800

    Initial Commit

---
+++ SwiftUI-T/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-T
+//
+//  Created by fengjutian on 2020/6/11.
+//  Copyright © 2020 fengjutian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
